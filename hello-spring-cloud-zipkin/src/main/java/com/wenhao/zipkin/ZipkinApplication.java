a27eda6da5120f3118052b90083c62bf61bfa549
beyond <921790476>
2019-08-30 17:31:19 +0800

    init

--- hello-spring-cloud-zipkin/src/main/java/com/wenhao/zipkin/ZipkinApplication.java
+++ hello-spring-cloud-zipkin/src/main/java/com/wenhao/zipkin/ZipkinApplication.java
@@ -1,14 +1,20 @@
 package com.wenhao.zipkin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import zipkin.server.internal.EnableZipkinServer;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import zipkin2.server.internal.EnableZipkinServer;
+
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZipkinServer
 public class ZipkinApplication {
+
+    @Autowired
+    CharacterEncodingFilter characterEncodingFilter;
 
     public static void main(String[] args) {
         SpringApplication.run(ZipkinApplication.class, args);
